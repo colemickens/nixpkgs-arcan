58440dae23ce8c796a5d78993b5d628bd7ebd959
Cole Mickens <cole.mickens@gmail.com>
2021-03-21 15:40:26 -0700

    wip

---
+++ arcan.nix
@@ -0,0 +1,260 @@
+{ lib, newScope, stdenv, fetchgit, pkgs }: let
+
+  # nicer aliases
+  derive = stdenv.mkDerivation;
+  concat = builtins.concatStringsSep " ";
+
+  # vendored libuvc: don't build, just make sources available
+  libuvc-src = derive {
+    name = "libuvc-src";
+    # using fetchgit instead fetchFromGitHub because
+    # the .git directory is needed by arcan's cmake scripts
+    src = fetchgit {
+      leaveDotGit = true;
+      url = "https://github.com/letoram/libuvc.git";
+      rev = "v0.0.6";
+      sha256 = "1jdmiinsd91nnli5hgcn9c6ifj0s6ngbyjwm0z6fim4f8krnh0sf";
+    };
+    nativeBuildInputs = with pkgs; [ git ];
+    # fetchgit strips all refs, leaving just a fetchgit branch
+    # but cmake needs to check out the ref called 'master':
+    installPhase = ''
+      git tag master
+      cp -r . $out/
+      cd $out
+    '';
+  };
+
+  # cmake flags pointing to locations of arcan headers
+  arcanIncludeDirs = arcan: [
+    "-DARCAN_SHMIF_INCLUDE_DIR=${arcan}/include/arcan/shmif"
+    "-DARCAN_TUI_INCLUDE_DIR=${arcan}/include/arcan"
+  ];
+
+  # cmake flags pointing to locations of libusb1 headers and binaries
+  libusbDirs = libusb1: [
+    "-DLIBUSB_1_INCLUDE_DIRS=${libusb1.dev}/include/libusb-1.0"
+    "-DLIBUSB_1_LIBRARIES=${libusb1}/lib/libusb-1.0.so"
+  ];
+
+in lib.makeScope newScope (self: with self; let
+
+  arcanRev = "0.6.0.1";
+
+  arcanCoreSrc = fetchgit {
+    leaveDotGit = true;
+    url = "https://github.com/letoram/arcan.git";
+    rev = arcanRev;
+    sha256 = "1hfqj39klbnh47lv1lbfs4l12kl58l5xkmdmzdc5fi4yg8h56njs";
+  };
+
+in {
+
+  # arcan core:
+
+  arcan = callPackage ({ pkgs }: derive {
+    name = "arcan";
+    src = arcanCoreSrc;
+    #patches = [ ./Arcan.nosuid.patch ]; # nix refuses to build suid binaries
+    postUnpack = '' # add vendored libuvc
+      mkdir -p ./arcan/external/git/libuvc
+      pushd ./arcan/external/git/
+      shopt -s dotglob nullglob  # bashism: * now also matches dotfiles
+      cp -r ${libuvc-src}/* libuvc/
+      shopt -u dotglob nullglob  # phases are stateful
+      popd
+    '';
+    patchPhase = '' # thanks @ChrisOboe!
+			sed -i "s,SETUID,,g" ./src/CMakeLists.txt
+			substituteInPlace ./src/platform/posix/paths.c \
+				--replace "/usr/bin" "$out/bin" \
+				--replace "/usr/share" "$out/share"
+    '';
+    nativeBuildInputs = with pkgs; [ cmake gcc git pkg-config ];
+    buildInputs = with pkgs; [
+      apr
+      espeak-classic
+      file
+      ffmpeg-full
+      freetype
+      harfbuzzFull
+      leptonica
+      libGL
+      libdrm
+      libjpeg
+      libusb1
+      libvncserver
+      libxkbcommon
+      luajit
+      lzma
+      mesa
+      openal
+      SDL2
+      sqlite
+      tesseract
+      vlc
+      wayland
+      wayland-protocols
+      xorg.libxcb
+      xorg.xcbutil
+      xorg.xcbutilwm
+    ];
+    PKG_CONFIG_PATH = concat [ # make wayland protocols available
+      "${pkgs.wayland-protocols}/share/pkgconfig"
+      "${pkgs.libusb1.dev}/lib/pkgconfig"
+    ];
+    CFLAGS = concat [ # don't warn on read()/write() without a format
+      "-Wno-format"   # (Arcan code uses them on SHMIFs)
+      "-Wno-format-security"
+    ];
+    cmakeFlags = concat (
+      # cmake won't be able to find these paths on its own:
+      (libusbDirs pkgs.libusb) ++ [
+      "-DDRM_INCLUDE_DIR=${pkgs.libdrm.dev}/include/libdrm"
+      "-DGBM_INCLUDE_DIR=${pkgs.libGL.dev}/include"
+      "-DWAYLANDPROTOCOLS_PATH=${pkgs.wayland-protocols}/share/wayland-protocols"
+      # enable features:
+      "-DVIDEO_PLATFORM=egl-dri"
+      "-DSHMIF_TUI_ACCEL=ON"
+      "-DENABLE_LWA=ON"
+      "-DNO_BUILTIN_OPENHMD=ON"
+      "-DHYBRID_SDL=On"
+      "-DHYBRID_HEADLESS=On"
+      "-DFSRV_DECODE_UVC=Off"
+      # optional
+      "-DVERBOSE=ON"
+      #"--debug-output"
+      #"--trace"
+      "../src"
+    ]);
+  }) {};
+
+  # arcan utilities:
+
+  acfgfs = callPackage ({ pkgs }: derive {
+    name = "acfgfs";
+    src = arcanCoreSrc;
+    nativeBuildInputs = with pkgs; [ cmake gcc git pkg-config ];
+    buildInputs = [ arcan ] ++ (with pkgs; [ fuse3 ]);
+    cmakeFlags = concat ((arcanIncludeDirs arcan) ++ [ "../src/tools/acfgfs" ]);
+  }) {};
+
+  aclip = callPackage ({ pkgs }: derive {
+    name = "aclip";
+    src = arcanCoreSrc;
+    nativeBuildInputs = with pkgs; [ cmake gcc git pkg-config ];
+    buildInputs = [ arcan ];
+    PKG_CONFIG_PATH = concat [ "${arcan}/lib/pkgconfig" ];
+    cmakeFlags = concat ((arcanIncludeDirs arcan) ++ [ "../src/tools/aclip" ]);
+  }) {};
+
+  aloadimage = callPackage ({ pkgs }: derive {
+    name = "aloadimage";
+    src = arcanCoreSrc;
+    nativeBuildInputs = with pkgs; [ cmake gcc git pkg-config ];
+    buildInputs = [ arcan ];
+    cmakeFlags = concat ((arcanIncludeDirs arcan) ++ [ "../src/tools/aloadimage" ]);
+  }) {};
+
+  shmmon = callPackage ({ pkgs }: derive {
+    name = "shmmon";
+    src = arcanCoreSrc;
+    nativeBuildInputs = with pkgs; [ cmake gcc git pkg-config ];
+    buildInputs = [ arcan ];
+    cmakeFlags = concat ((arcanIncludeDirs arcan) ++ [ "../src/tools/shmmon" ]);
+  }) {};
+
+  # TODO: provide <hidapi/hidapi.h> include path
+  #vrbridge = callPackage ({ pkgs }: derive {
+    #name = "vrbridge";
+    #src = ./arcan;
+    #nativeBuildInputs = with pkgs; [ cmake gcc git pkg-config ];
+    #buildInputs = [ arcan ] ++ (with pkgs; [ libusb1 ]);
+    #cmakeFlags = concat (
+      #(arcanIncludeDirs arcan) ++
+      #(libusbDirs pkgs.libusb1) ++
+      #[ "../src/tools/vrbridge" ]
+    #);
+  #}) {};
+
+} // (let
+
+  mkArcanAppl = { name, src, applRoot }: callPackage (
+    { bash
+    , pkg-config
+    }: derive {
+      name = name;
+      src = src;
+      nativeBuildInputs = [ pkg-config ];
+      buildInputs = [ arcan ];
+      installPhase = ''
+
+        mkdir -p $out/bin $out/share/${name} $out/share/wayland-sessions
+
+        # add appl code
+        cp -r ./${applRoot}/* $out/share/${name}/
+
+        # create executable wrapper
+        echo -e "#!${bash}/bin/bash\nexec /run/wrappers/bin/arcan $out/share/${name}" \
+          > $out/bin/${name}
+        chmod +x $out/bin/${name}
+
+        # create session wrapper
+        echo -e "[Desktop Entry]\nName=${name} on Arcan ${arcanRev}\nComment=Next Generation Window Manager\nExec=$out/bin/durden\nType=Application" \
+          > $out/share/wayland-sessions/${name}.desktop
+        chmod +x $out/share/wayland-sessions/${name}.desktop
+
+      '';
+      passthru.providedSessions = [ name ];
+    }
+  ) {};
+
+in {
+
+  # arcan appls
+
+  awb = mkArcanAppl {
+    name = "awb";
+    src = fetchgit {
+      leaveDotGit = true;
+      url    = "https://github.com/letoram/awb.git";
+      rev    = "271ef7ffd7f24569d2f836198e4c96b8c617e372";
+      sha256 = "0g6g493ygabhwfknjlp2rg14iq7njsgh5qll01p2g1i9qvwbbhvq";
+    };
+    applRoot = "";
+  };
+
+  prio = mkArcanAppl {
+    name = "prio";
+    src = fetchgit {
+      leaveDotGit = true;
+      url = "https://github.com/letoram/prio.git";
+      rev = "c3f97491339d15f063d6937d5f89bcfaea774dd1";
+      sha256 = "0igsbzp0df24f856sfwzcgcfanxlvxmw5v94gqq2p42kwardfmm9";
+    };
+    applRoot = "";
+  };
+
+  durden = mkArcanAppl {
+    name = "durden";
+    src = fetchgit {
+      leaveDotGit = true;
+      url = "https://github.com/letoram/durden.git";
+      rev = "bfbfe68bc325a5fb06ea1869a99404e277291a89";
+      sha256 = "0d5g161xaccq3jxci9vwr2xn51ilrcdh3a9sdlsw3d64a1f4x4rv";
+    };
+    applRoot = "durden";
+  };
+
+  /*safespaces = mkArcanAppl {
+    name = "safespaces";
+    src = fetchgit {
+      leaveDotGit = true;
+      url = "https://github.com/letoram/safespaces.git";
+      rev = "https://github.com/letoram/safespaces/commit/58eef59afba091293cab4d2b156e725f75d92eaf";
+      sha256 = "1jdmiinsd91nnli5hgcn9c6ifj0s6ngbyjwm0z6fim4f8krnh0s9";
+    };
+    applRoot = "safespaces";
+  };*/
+
+}))
--- flake.nix
+++ flake.nix
@@ -34,93 +34,25 @@
       defaultPackage = packagesBundle;
 
       packages = forAllSystems (system:
-        fullPkgs_.nixpkgs.${system}.waylandPkgs);
+        fullPkgs_.nixpkgs.${system}.arcanPkgs);
 
       smallPackages = forAllSystems (system:
-        fullPkgs_.unstableSmall.${system}.waylandPkgs);
+        fullPkgs_.unstableSmall.${system}.arcanPkgs);
 
       packagesBundle = forAllSystems (system: with fullPkgs_.nixpkgs.${system};
           linkFarmFromDrvs "wayland-packages-unstable"
-            (builtins.attrValues waylandPkgs));
+            (builtins.attrValues arcanPkgs));
 
       smallPackagesBundle = forAllSystems (system: with fullPkgs_.unstableSmall.${system};
           linkFarmFromDrvs "wayland-packages-unstable-small"
-            (builtins.attrValues waylandPkgs));
+            (builtins.attrValues arcanPkgs));
 
       overlay = final: prev:
         let
-          waylandPkgs = rec {
-            # wlroots-related
+          arcanPkgs = rec {
             cage             = prev.callPackage ./pkgs/cage { wlroots = prev.wlroots; };
-            drm_info         = prev.callPackage ./pkgs/drm_info {};
-            gebaar-libinput  = prev.callPackage ./pkgs/gebaar-libinput {};
-            glpaper          = prev.callPackage ./pkgs/glpaper {};
-            grim             = prev.callPackage ./pkgs/grim {};
-            kanshi           = prev.callPackage ./pkgs/kanshi {};
-            imv              = prev.callPackage ./pkgs/imv {
-              imv = prev.imv;
-            };
-            lavalauncher     = prev.callPackage ./pkgs/lavalauncher {};
-            mako             = prev.callPackage ./pkgs/mako {};
-            nwg-launchers    = prev.callPackage ./pkgs/nwg-launchers {};
-            oguri            = prev.callPackage ./pkgs/oguri {};
-            rootbar          = prev.callPackage ./pkgs/rootbar {};
-            slurp            = prev.callPackage ./pkgs/slurp {};
-            sway-unwrapped   = prev.callPackage ./pkgs/sway {
-              wayland = inputs.unstableSmall.legacyPackages.${prev.system}.wayland;
-            };
-            swaybg           = prev.callPackage ./pkgs/swaybg {};
-            swayidle         = prev.callPackage ./pkgs/swayidle {};
-            swaylock         = prev.callPackage ./pkgs/swaylock {};
-            waybar           = prev.callPackage ./pkgs/waybar {};
-            waypipe          = prev.callPackage ./pkgs/waypipe {};
-            wayvnc           = prev.callPackage ./pkgs/wayvnc {};
-            wlvncc           = prev.callPackage ./pkgs/wlvncc {
-              libvncserver = libvncserver_master;
-            };
-            wdisplays        = prev.callPackage ./pkgs/wdisplays {};
-            wev              = prev.callPackage ./pkgs/wev {};
-            wf-recorder      = prev.callPackage ./pkgs/wf-recorder {};
-            wlay             = prev.callPackage ./pkgs/wlay {};
-            obs-wlrobs       = prev.callPackage ./pkgs/obs-wlrobs {};
-            wl-clipboard     = prev.callPackage ./pkgs/wl-clipboard {};
-            wl-gammactl      = prev.callPackage ./pkgs/wl-gammactl {};
-            wldash           = prev.callPackage ./pkgs/wldash {};
-            wlogout          = prev.callPackage ./pkgs/wlogout {};
-            wlroots          = prev.callPackage ./pkgs/wlroots {
-              wayland = inputs.unstableSmall.legacyPackages.${prev.system}.wayland;
-            };
-            wlr-randr        = prev.callPackage ./pkgs/wlr-randr {};
-            wlsunset         = prev.callPackage ./pkgs/wlsunset {};
-            wofi             = prev.callPackage ./pkgs/wofi {};
-            wtype            = prev.callPackage ./pkgs/wtype {};
-            xdg-desktop-portal-wlr = prev.callPackage ./pkgs/xdg-desktop-portal-wlr {};
-            # misc
-            aml = prev.callPackage ./pkgs/aml {};
-            clipman = prev.callPackage ./pkgs/clipman {};
-            dunst = prev.callPackage ./pkgs/dunst {
-              dunst = prev.dunst;
-            };
-            gtk-layer-shell = prev.callPackage ./pkgs/gtk-layer-shell {};
-            i3status-rust    = prev.callPackage ./pkgs/i3status-rust {};
-            neatvnc = prev.callPackage ./pkgs/neatvnc {};
-            obs-studio = prev.libsForQt5.callPackage ./pkgs/obs-studio {
-              ffmpeg = prev.ffmpeg_4;
-              vlc = prev.vlc;
-            };
-            obs-xdg-portal = prev.callPackage ./pkgs/obs-xdg-portal {};
-            wlfreerdp = prev.callPackage ./pkgs/wlfreerdp {
-              inherit (prev) libpulseaudio;
-              inherit (prev.gst_all_1) gstreamer gst-plugins-base gst-plugins-good;
-            };
-            # wayfire stuff
-            wayfire          = prev.callPackage ./pkgs/wayfire {};
-
-            libvncserver_master = prev.callPackage ./pkgs/libvncserver_master {
-              libvncserver = prev.libvncserver;
-            };
           };
         in
-          waylandPkgs // { inherit waylandPkgs; };
+          arcanPkgs // { inherit arcanPkgs; };
     };
 }
